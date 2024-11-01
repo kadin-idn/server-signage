7574449c735219474f55e00b900e6e3693ce21f9
rahma aulia <qwertyrahmaaulia@gmail.com>
2024-11-01 16:09:52 +0700

    feat : upload file hero banner

--- controllers/heroBanner.js
+++ controllers/heroBanner.js
@@ -1,12 +1,41 @@
+const { HeroBanner, File } = require("../models");
 class ControllerHeroBanner {
- static async createHeroBanner(req, res) {
+  static async createHeroBanner(req, res) {
     try {
-        
+      const { title, description } = req.body;
+      if (!req.files) throw { name: "FileIsEmpty" };
+      const file = req.files.fileHero;
+      console.log(file);
+
+      if (!file) throw { name: "FileIsEmpty" };
+
+      const filename = file.name;
+
+      const moveFile = (file, path) => {
+        return new Promise((resolve, reject) => {
+          file.mv(path, function (err) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        });
+      };
+
+      await Promise.all([
+        moveFile(file, __dirname + ".." + "/uploads/" + filename),
+      ]);
+      res.send("Files Uploaded");
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      if (error.name === "FileIsEmpty") {
+        res.status(400).json({ message: "File is empty" });
+      } else {
         res.status(500).json({ message: "Internal server error" });
+      }
     }
- }
+  }
 }
 
 module.exports = ControllerHeroBanner;
--- routes/heroBanner.js
+++ routes/heroBanner.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const ControllerHeroBanner = require('../controllers/heroBanner');
-const Authentication = require('../middlewares/authentication');
-const { Authorization } = require('../middlewares/autthorization');
-const HeroBanner = express.Router()
+const express = require("express");
+const ControllerHeroBanner = require("../controllers/heroBanner");
+const Authentication = require("../middlewares/authentication");
+const { Authorization } = require("../middlewares/autthorization");
+const fileUpload = require("express-fileupload");
+const HeroBanner = express.Router();
 
-HeroBanner.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-HeroBanner.post('/', Authentication, Authorization, ControllerHeroBanner.createHeroBanner)
-
-module.exports = HeroBanner
+HeroBanner.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+HeroBanner.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 }, // Batas ukuran file 50MB
+    abortOnLimit: true,
+    responseOnLimit: "File size limit has been reached",
+  })
+);
+HeroBanner.post(
+  "/",
+  ControllerHeroBanner.createHeroBanner
+);
+module.exports = HeroBanner;
